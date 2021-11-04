b617a97d7a15c349aeb7f37f665224980783f6ba
Wouter Boereboom <wouter.boereboom@adyen.com>
2021-11-04 13:01:35 +0100

    added adyenHelper data to template in BM cartridge

--- src/cartridges/bm_adyen/cartridge/controllers/AdyenSettings.js
+++ src/cartridges/bm_adyen/cartridge/controllers/AdyenSettings.js
@@ -2,10 +2,22 @@
 
 const server = require('server');
 const csrfProtection = require('*/cartridge/scripts/middleware/csrf');
+const AdyenHelper = require('*/cartridge/scripts/util/adyenHelper');
 
 server.get('Start', csrfProtection.generateToken, function (req, res, next) {
   res.render('helloWorld', {
-    text: "I've been passed down from the controller!"
+    text: "I've been passed down from the controller!",
+    environment: AdyenHelper.getAdyenEnvironment(),
+    merchantAccount: AdyenHelper.getAdyenMerchantAccount(),
+  });
+  return next();
+});
+
+server.get('getSetting', csrfProtection.generateToken, function (req, res, next) {
+  res.render('helloWorld', {
+    text: "I've been passed down from the controller via the button!",
+    environment: AdyenHelper.getAdyenEnvironment(),
+    merchantAccount: AdyenHelper.getAdyenMerchantAccount(),
   });
   return next();
 });
